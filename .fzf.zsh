0a05656cab6e5252fca2a3e4e097b4a3d1093684
Mark St.Godard <markstgodard@gmail.com>
2015-09-30 10:23:54 -0500

    Add fzf

---
+++ .fzf.zsh
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" =~ "/Users/mark/.fzf/bin" ]]; then
+  export PATH="$PATH:/Users/mark/.fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" =~ "/Users/mark/.fzf/man" && -d "/Users/mark/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/Users/mark/.fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- =~ i ]] && source "/Users/mark/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/mark/.fzf/shell/key-bindings.zsh"
+
