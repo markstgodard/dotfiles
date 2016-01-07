9825e901dd20f785e827926405956ebb9786db77
Mark St.Godard <markstgodard@gmail.com>
2016-01-07 10:58:29 -0600

    Add CF specific env varz

---
+++ zsh/cf.zsh
@@ -0,0 +1,7 @@
+# env variables related to CF dev
+
+export DIEGO_RELEASE_DIR=~/workspace/diego-release
+
+export BBS_ENDPOINT=https://10.244.16.130:8889
+export BBS_CERT_FILE=$DIEGO_RELEASE_DIR/manifest-generation/bosh-lite-stubs/bbs-certs/client.crt
+export BBS_KEY_FILE=$DIEGO_RELEASE_DIR/manifest-generation/bosh-lite-stubs/bbs-certs/client.key
