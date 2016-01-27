36bc27ab64a2d0e4035d9a0b8000bf24077e936a
Mark St.Godard <markstgodard@gmail.com>
2016-01-27 09:54:48 -0600

    Add function for logging into local bosh-lite

--- zsh/cf.zsh
+++ zsh/cf.zsh
@@ -5,3 +5,13 @@ export DIEGO_RELEASE_DIR=~/workspace/diego-release
 export BBS_ENDPOINT=https://10.244.16.130:8889
 export BBS_CERT_FILE=$DIEGO_RELEASE_DIR/manifest-generation/bosh-lite-stubs/bbs-certs/client.crt
 export BBS_KEY_FILE=$DIEGO_RELEASE_DIR/manifest-generation/bosh-lite-stubs/bbs-certs/client.key
+
+
+# cf login to local bosh lite
+function cf_bosh_lite {
+  if (( $# == 0 ))
+    then echo usage: cf_bosh_lite password;
+  else
+    cf api api.bosh-lite.com --skip-ssl-validation && cf auth admin $1
+  fi
+}
