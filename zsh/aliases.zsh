a8e6216025849ccf136690651736fccace43097d
Mark St.Godard <markstgodard@gmail.com>
2016-01-28 10:26:30 -0600

    Add aliases to deploy cf, diego and routing release

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,4 +7,6 @@ alias h="history"
 alias ginko="ginkgo"
 
 # Cloud Foundry
-alias dammit='bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-dev-manifest && bosh -n upload release && bosh -n deploy'
+alias dammit='cd ~/workspace/cf-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-dev-manifest && bosh -n upload release && bosh -n deploy'
+alias dammit-diego='cd ~/workspace/diego-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifests && bosh -n upload release && bosh -n deploy'
+alias dammit-routing='cd ~/workspace/cf-routing-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifests && bosh -n upload release && bosh -n deploy'
