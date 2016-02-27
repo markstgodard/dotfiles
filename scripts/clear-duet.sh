8ecc32f18888caca2123bcd8b7c97397427e03b7
Mark St.Godard <markstgodard@gmail.com>
2016-02-27 17:09:19 -0600

    hack

---
+++ scripts/clear-duet.sh
@@ -0,0 +1,6 @@
+git config --unset duet.env.git-author-name
+git config --unset duet.env.git-author-initials
+git config --unset duet.env.git-author-email
+git config --unset duet.env.git-commiter-name
+git config --unset duet.env.git-commiter-initials
+git config --unset duet.env.git-commiter-email1
