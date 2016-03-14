182e3fcd247a2302ffb77d0d48e035c70ba7aaec
Mark St.Godard <markstgodard@gmail.com>
2016-03-14 13:05:54 -0500

    Update alias for cf routing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,4 +11,4 @@ alias gink="ginkgo"
 # Cloud Foundry
 alias dammit='cd ~/workspace/cf-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-dev-manifest && bosh -n upload release && bosh -n deploy'
 alias dammit-diego='cd ~/workspace/diego-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifests && bosh -n upload release && bosh -d ~/workspace/diego-release/bosh-lite/deployments/diego.yml -n deploy'
-alias dammit-routing='cd ~/workspace/cf-release/src/cf-routing-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifest && bosh -n upload release && bosh -d ~/workspace/cf-release/src/cf-routing-release/bosh-lite/deployments/cf-routing-manifest.yml -n deploy'
+alias dammit-routing='cd ~/workspace/cf-routing-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifest && bosh -n upload release && bosh -d ~/workspace/cf-routing-release/bosh-lite/deployments/cf-routing-manifest.yml -n deploy'
