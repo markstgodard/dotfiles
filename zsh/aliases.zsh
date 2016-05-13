9e02d0a495011316ab85faf3b61b999643d8a951
Mark St.Godard <markstgodard@gmail.com>
2016-05-13 13:34:14 -0500

    remove alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,7 +10,6 @@ alias gink="ginkgo"
 # Cloud Foundry
 alias dammit='cd ~/workspace/cf-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-dev-manifest && bosh -n upload release && bosh -n deploy'
 alias dammit-diego='cd ~/workspace/diego-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifests && bosh -n upload release && bosh -d ~/workspace/diego-release/bosh-lite/deployments/diego.yml -n deploy'
-alias dammit-routing='cd ~/workspace/cf-routing-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifest && bosh -n upload release && bosh -d ~/workspace/cf-routing-release/bosh-lite/deployments/cf-routing-manifest.yml -n deploy'
 
 alias igiveup='cd ~/workspace/bosh-lite && vagrant halt && vagrant destroy && vagrant up && dammit'
 
