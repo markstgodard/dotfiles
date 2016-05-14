4cc12bf622fd9bfbc904c5b497459be2b5b7c6b0
Mark St.Godard <markstgodard@gmail.com>
2016-05-14 16:37:29 -0500

    cleanup aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,12 +7,8 @@ alias ginko="ginkgo"
 alias gingo="ginkgo"
 alias gink="ginkgo"
 
-# Cloud Foundry
-alias dammit='cd ~/workspace/cf-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-dev-manifest && bosh -n upload release && bosh -n deploy'
-alias dammit-diego='cd ~/workspace/diego-release && bosh create release --force && bosh target lite && ./scripts/generate-bosh-lite-manifests && bosh -n upload release && bosh -d ~/workspace/diego-release/bosh-lite/deployments/diego.yml -n deploy'
-
+# when all else fails
 alias igiveup='cd ~/workspace/bosh-lite && vagrant halt && vagrant destroy && vagrant up && dammit'
-
 
 # dot files
 alias dots='cd ~/dotfiles && ./scripts/update-dotfiles'
