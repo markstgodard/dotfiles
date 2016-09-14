737fe5acc233763e60f6293dceea18263e04f81a
Mark St.Godard <markstgodard@gmail.com>
2016-09-14 10:29:05 -0500

    cleanup

--- scripts/dev-netman.sh
+++ scripts/dev-netman.sh
@@ -4,13 +4,13 @@ WORKDIR=$HOME'/workspace/netman-release'
 tmux new-session -n 'dev' -c $WORKDIR -d -s $SESSION_NAME
 tmux send-keys -t $SESSION_NAME 'vim .' C-m
 tmux split-window -h -p 30 -c $WORKDIR
-tmux send-keys -t $SESSION_NAME '# run ./scripts/docker-test or ginkgo here' C-m
+tmux send-keys -t $SESSION_NAME './scripts/docker-test src/lib' C-m
 
 tmux new-window -n "test" -c $WORKDIR
-tmux send-keys -t $SESSION_NAME:2 '# run adhoc tests here' C-m
+tmux send-keys -t $SESSION_NAME:2 '# run adhoc tests, manual CF tests, etc. here' C-m
 
 tmux new-window -n "bosh" -c $WORKDIR
-tmux send-keys -t $SESSION_NAME:3 '# do bosh stuff here' C-m
+tmux send-keys -t $SESSION_NAME:3 '# do BOSH stuff here' C-m
 
 tmux new-window -n "logs" -c $WORKDIR
 tmux send-keys -t $SESSION_NAME:4 '# use for tailing logs, etc' C-m
