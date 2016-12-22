9cf337edf41bad5988b30740153d36d5d034db00
Mark St.Godard <markstgodard@gmail.com>
2016-12-22 08:54:16 -0600

    cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,14 +7,4 @@ alias ginko="ginkgo"
 alias gingo="ginkgo"
 alias gink="ginkgo"
 
-# when all else fails
-alias igiveup='cd ~/workspace/bosh-lite && vagrant halt && vagrant destroy && vagrant up && dammit'
-
-# dot files
-alias dots='cd ~/dotfiles && ./scripts/update-dotfiles'
-
 alias goal='gaol'
-
-alias b='bosh'
-
-#alias rake="noglob rake"
